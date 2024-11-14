29944fcdc99914c801b504850817715df8b35334
Carlos Vazquez <carlosangel2710@hotmail.com>
2024-11-14 00:31:32 -0600

    Stephanie Martinez Muñoz

--- LibrisB4Estefani/src/App.java
+++ LibrisB4Estefani/src/App.java
@@ -2,7 +2,7 @@ import Service.BookService;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Bienvenido a la biblioteca");
+        System.out.println("Bienvenido a la biblioteca"); /* Imprime el mensaje de bienvenida */
         BookService book = new BookService();
         book.registerBook();
     }
